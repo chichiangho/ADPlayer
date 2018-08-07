1955ca3bf3e74d311a5aca8cdfd9ce6afda68377
chichiangho <583021389@qq.com>
2018-08-07 10:56:28 +0800

    asgasfg

--- app/src/main/java/com/adplayer/activity/MainActivity.kt
+++ app/src/main/java/com/adplayer/activity/MainActivity.kt
@@ -68,9 +68,9 @@ class MainActivity : BaseActivity() {
             }
 
             override fun surfaceDestroyed(holder: SurfaceHolder?) {
-//                mCamera?.stopPreview()
-//                mCamera?.unlock()
-//                mCamera?.release()
+                mCamera?.stopPreview()
+                mCamera?.unlock()
+                mCamera?.release()
             }
 
             override fun surfaceCreated(holder: SurfaceHolder?) {
@@ -84,7 +84,7 @@ class MainActivity : BaseActivity() {
                             mCamera?.let {
                                 it.setPreviewDisplay(holder)
                                 setCameraDisplayOrientation(this@MainActivity, i, it)
-                                it.setDisplayOrientation(0)
+                                it.setDisplayOrientation(90)
                                 it.startPreview()
                             }
                         }
@@ -106,12 +106,12 @@ class MainActivity : BaseActivity() {
                 .start()
 
         ConnectManager.init()
-        OnLineReceiver.addOnLineListener(object : OnLineReceiver.ChangeListener {
-            override fun onLine() {
-                ConnectManager.stop()
-                ConnectManager.init()
-            }
-        })
+//        OnLineReceiver.addOnLineListener(object : OnLineReceiver.ChangeListener {
+//            override fun onLine() {
+//                ConnectManager.stop()
+//                ConnectManager.init()
+//            }
+//        })
         ConnectManager.registerCommandListener { command, params, result ->
             runOnUiThread {
                 when (command) {
@@ -160,41 +160,53 @@ class MainActivity : BaseActivity() {
         taking = true
         val path = appCtx.getExternalFilesDir("takePhoto").absolutePath + "/" + System.currentTimeMillis() + ".png"
 
-        mCamera?.setOneShotPreviewCallback { bytes: ByteArray, camera: Camera ->
-            val size = camera.parameters.previewSize // 获取预览大小，若生成图片给的高宽和预览的高宽不一致，会导致生成的预览图出现花图的现象
-            val w = size.width // 宽度
-            val h = size.height
-            val image = YuvImage(bytes, ImageFormat.NV21, w, h, null)
-            val os = ByteArrayOutputStream()
+        try {
+            mCamera?.setOneShotPreviewCallback { bytes: ByteArray, camera: Camera ->
+                val size = camera.parameters.previewSize // 获取预览大小，若生成图片给的高宽和预览的高宽不一致，会导致生成的预览图出现花图的现象
+                val w = size.width // 宽度
+                val h = size.height
+                val image = YuvImage(bytes, ImageFormat.NV21, w, h, null)
+                val os = ByteArrayOutputStream()
 
-            if (!image.compressToJpeg(Rect(0, 0, w, h), 100, os)) {
-                return@setOneShotPreviewCallback
-            }
-            val temp = os.toByteArray()
-
-            val source = BitmapFactory.decodeByteArray(temp, 0, temp.size)
-            val file = File(path)
-            if (!file.exists())
-                file.createNewFile()
-            var out: FileOutputStream? = null
-            try {
-                out = FileOutputStream(file)
-                source.compress(Bitmap.CompressFormat.JPEG, 90, out)
-                result(ResultJSON(ResultJSON.TAKE_PIC, path))
-            } catch (e: FileNotFoundException) {
-                e.printStackTrace()
-                result(ResultJSON(ResultJSON.NO_SUCH_FILE))
-            } finally {
-                try {
-                    out?.flush()
-                    out?.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
+                if (!image.compressToJpeg(Rect(0, 0, w, h), 100, os)) {
+                    return@setOneShotPreviewCallback
                 }
+                val temp = os.toByteArray()
+
+                val source1 = BitmapFactory.decodeByteArray(temp, 0, temp.size)
+
+                val matrix = Matrix()
+                matrix.postRotate(-90F)
+                val source = Bitmap.createBitmap(source1, 0, 0, source1.width, source1.height, matrix, true)
+
+                val file = File(path)
+                if (!file.exists())
+                    file.createNewFile()
+                var out: FileOutputStream? = null
+                try {
+                    out = FileOutputStream(file)
+                    source.compress(Bitmap.CompressFormat.JPEG, 90, out)
+                    result(ResultJSON(ResultJSON.TAKE_PIC, path))
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                    result(ResultJSON(ResultJSON.NO_SUCH_FILE))
+                } finally {
+                    try {
+                        out?.flush()
+                        out?.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                taking = false
+                file.delete()
+                mCamera?.stopPreview()
+            } ?: let {
+                result(ResultJSON(ResultJSON.CAMERA_NOT_READY))
+                taking = false
             }
-            taking = false
-            file.delete()
-        } ?: let {
+            mCamera?.startPreview()
+        } catch (e: Exception) {
             result(ResultJSON(ResultJSON.CAMERA_NOT_READY))
         }
     }
